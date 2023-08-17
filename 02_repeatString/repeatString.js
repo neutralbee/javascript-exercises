b8616350b35cfbe4dc0f3836f148abadb5de54d6
Replit user <>
2023-08-17 15:30:06 +0000

    Completes 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let outString = "";
+  if (num<0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      outString += string;
+    }
+    return outString;
+  }
 };
 
 // Do not edit below this line
