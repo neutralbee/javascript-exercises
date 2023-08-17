04f53137e34aca0348934a28c5964e4bd8a20a2f
Replit user <>
2023-08-17 16:53:59 +0000

    Completes 04_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+  let stringLength = word.length;
+  const stringArray = [];
 
+  for (let i = stringLength; i >= 0; i--) {
+    stringArray.push(word.charAt(i));
+  }
+
+  return stringArray.join("");
 };
 
 // Do not edit below this line
