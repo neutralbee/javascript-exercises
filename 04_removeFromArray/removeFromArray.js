909f6f6f26c5f7b8dffe8084728b6108d522704b
Replit user <>
2023-08-22 17:54:59 +0000

    Pass test 1 of 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...unwantedItems) {
+let outputArray = new Array();
+  
+  for (const searchItem of unwantedItems) {
+    for (const arrayItem of inputArray) {
+      if (searchItem === arrayItem) {
+        continue
+      } else {
+        outputArray.push(arrayItem);
+      }
+    }
+  }
+return outputArray;
 };
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray; 
