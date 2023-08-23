c679f8e6afc7dbc8c07baab2be06bd4bbff2b379
Replit user <>
2023-08-23 18:16:28 +0000

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  tempC = (tempF - 32) * (5 / 9);
+  return parseFloat(tempC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  tempF = (tempC * (9 / 5) + 32);
+  return parseFloat(tempF.toFixed(1));
 };
 
 // Do not edit below this line
